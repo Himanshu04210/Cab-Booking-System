6ad3b1d2bf17f8d982fa2ab135ea89f1de0295ab
Himanshu04210 <himanshupoly76075@gmail.com>
2023-06-29 17:39:55 +0530

    creating user detail service implementation

---
+++ Cab_Booking_System/src/main/java/com/masai/Security/UserDetailServiceImple.java
@@ -0,0 +1,50 @@
+package com.masai.Security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.masai.Repository.DriverRepository;
+import com.masai.Repository.UserRepository;
+import com.masai.model.Users;
+
+public class UserDetailServiceImple implements UserDetailsService{
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private DriverRepository driverRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		
+		
+		 Optional<Users> optUser = userRepository.findByEmail(username);
+		 
+		 if(optUser.isPresent()) {
+			 
+		 }
+		 
+		 
+		 Optional<Users> optDriver = driverRepository.findByEmail(username);
+		 
+		 if(optDriver.isPresent()) {
+			 
+		 }
+		 
+		 
+		 
+		 
+		
+		return null;
+	}
+	
+	
+	
+
+}
