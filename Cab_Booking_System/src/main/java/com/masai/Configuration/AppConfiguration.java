88fa1803c8ef09956b8daa5fcf134aa5c03d070e
Himanshu04210 <himanshupoly76075@gmail.com>
2023-07-19 00:45:57 +0530

    removing unneccesry code

--- Cab_Booking_System/src/main/java/com/masai/Configuration/AppConfiguration.java
+++ Cab_Booking_System/src/main/java/com/masai/Configuration/AppConfiguration.java
@@ -1,6 +1,8 @@
 package com.masai.Configuration;
 
-import jakarta.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -14,14 +16,13 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.Collections;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class AppConfiguration {
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		
 
         http
@@ -74,7 +75,7 @@ public class AppConfiguration {
 	 
 	
 	@Bean
-	public PasswordEncoder passwordEncoder() {
+	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
--- Cab_Booking_System/src/main/java/com/masai/model/Transactions.java
+++ Cab_Booking_System/src/main/java/com/masai/model/Transactions.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
