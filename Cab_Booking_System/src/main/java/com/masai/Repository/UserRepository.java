103fdc9d586d8bdb89cf184ea52137e7b56b985a
Himanshu04210 <himanshupoly76075@gmail.com>
2023-08-17 12:38:49 +0530

    enhance one more functionality

--- Cab_Booking_System/src/main/java/com/masai/Repository/UserRepository.java
+++ Cab_Booking_System/src/main/java/com/masai/Repository/UserRepository.java
@@ -3,10 +3,11 @@ package com.masai.Repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.masai.model.Users;
 
-public interface UserRepository extends JpaRepository<Users, Integer>{
+public interface UserRepository extends JpaRepository<Users, Integer>, PagingAndSortingRepository<Users, Integer>{
 
 	Optional<Users> findByEmail(String email);
 	
